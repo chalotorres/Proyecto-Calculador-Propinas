675f5646a736f36664e44e5a452c67584aeadbb9
Hazael Torres <hazaeltorres09@gmail.com>
2025-11-15 14:28:30 -0600

    Mostrar orden

---
+++ src/components/OrderContents.tsx
@@ -0,0 +1,37 @@
+import { formatCurrency } from "../helpers"
+import type { OrderItem } from "../types"
+
+type OrderContentsProps = {
+    order : OrderItem[]
+}
+
+export default function orderContents({order} : OrderContentsProps) {
+  return (
+    <div>
+        <h2 className='font-black text-3xl'>Consumo</h2>
+    
+        <div className="space-y-3 mt-10">
+            {order.length === 0 ?
+                <p className="text-center">La orden está vacía</p>
+                :(
+                    order.map (item => (
+                        <div key={item.id}
+                        className="flex justify-between border-t items-center border-gray-300 py-5 last-of-type:border-b">
+                            <div>
+                                <p className="text-lg">
+                                    {item.name} - {formatCurrency(item.price)}
+                                </p>
+                                <p className="font-black">
+                                    Cantidad: {item.quantity} - {formatCurrency(item.price * item.quantity)}
+                                </p>
+                            </div>
+                            <button className="bg-red-600 h-8 w-8 rounded-full font-black text-white">
+                                X
+                            </button>
+                        </div>
+                    ))
+                ) }
+        </div>
+    </div>
+  )
+}
