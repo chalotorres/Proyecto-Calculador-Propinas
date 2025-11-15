c1f683b508c1b243b7d52bf80df5d2f81b2070ec
Hazael Torres <hazaeltorres09@gmail.com>
2025-11-15 12:31:31 -0600

    Primer commit proyecto calculadora de propinas

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { menuItems } from "./data/db"
+
+
+function App() {
+
+  console.log(menuItems);
+
+  return (
+    <>
+    <header className=" bg-teal-700 py-5">
+      <h1 className="text-center text-4xl font-black text-white">Calculadora de Propinas y Consumo</h1>
+    </header>
+
+    <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+      
+      <div>
+        <h2>Menú</h2>
+      </div>
+
+      <div>
+        <h2>Consumo</h2>
+      </div>
+
+    </main>
+    </>
+  )
+}
+
+export default App
